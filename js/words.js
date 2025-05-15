961780829baeb9ae769ae9e0a7653930c51d6877
Saniya Malik <168172365+saaniyamalik@users.noreply.github.com>
2025-05-16 02:09:46 +0500

    Add files via upload

---
+++ js/words.js
@@ -0,0 +1,7 @@
+var words = new Array();
+function getRandomWord() {
+    var idx = Math.random();
+    idx = parseInt(100 * idx % words.length);
+    word = "I don't know what's happened to me to my heart since I meet you. The way my feelings are increasing day by day for you. You are the only one for me in the whole universe لفظ نہیں جو بیاں کر سکیں کے مجھے تم سے کتنی محبت ہے"
+    return word;
+}
